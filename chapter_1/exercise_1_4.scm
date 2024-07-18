46c2c2afaee823817df13434d0594f15fcf3a9f9
Sofia K. <sophiakirpichenok@gmail.com>
2024-07-18 23:07:36 +0200

    added gitignore

--- chapter_1/exercise_1_4.scm
+++ chapter_1/exercise_1_4.scm
@@ -1,1 +1,5 @@
 ;;; this function take necessary operator: if b < 0, it takes - and + otherwise.
+
+(define (a_plus_asb-b a b)
+    ((if (> b 0) + -) a b)
+)
--- chapter_1/exercise_1_5.scm
+++ chapter_1/exercise_1_5.scm
@@ -1,3 +1,11 @@
+;;; If Ben uses applicative-order evaluation (AOE), he will get an error because the program will
+;;; try to evaluate all arguments before applying the function. In this case, evaluating (p) leads
+;;; to an infinite recursion. and Scheme Lisp uses applicative-order evaluation, so it will result
+;;; in an error due to the recursive call in (p).
+
+;;; If Ben uses normal-order evaluation (NOE), he will get 0. This is because, with NOE, arguments
+;;; are only evaluated when needed. Since x is 0, the if condition will return 0 without evaluating (p).
+
 (define (p) (p))
 
 (define (test x y)
