c6122cfbaad66b3c684237164e71339ce0b8121a
Sofia K. <sophiakirpichenok@gmail.com>
2024-08-07 12:28:02 +0200

    completed 1_22

--- chapter_1/exercise_1_22.scm
+++ chapter_1/exercise_1_22.scm
@@ -1,18 +1,62 @@
 #|
 - first 3 after 1000:
-1009
-1013
-1019
+1009 *** 0.
+1013 *** 0.
+1019 *** 0.
 
 - first 3 after 10000:
-10007
-10009
-10037
+10007 *** 0.
+10009 *** 0.
+10037 *** 0.
 
 - first 3 after 100000:
-100003
-100019
-100043
+100003 *** 0.
+100019 *** 0.
+100043 *** 0.
+
+- first 3 after 1000000:
+1000003 *** 0.
+1000033 *** 0.
+1000037 *** 0.
+
+For my laptop this calculations are too fast, so I don't see the difference and runtime = 0 every time.
+That is why I tried bigger numbers:
+
+1.000.000.000:
+1000000007 *** 0.02
+1000000009 *** 0.03
+1000000021 *** 0.03
+
+10.000.000.000:
+10000000019 *** 0.08
+10000000033 *** 0.08
+10000000061 *** 0.08
+
+100.000.000.000:
+100000000003 *** 0.27
+100000000019 *** 0.26
+100000000057 *** 0.26
+
+1.000.000.000.000:
+1000000000039 *** 0.76
+1000000000061 *** 0.78
+1000000000063 *** 0.76
+
+1. Should checking the primality of numbers near 10.000.000.000 take √10 times longer than for 
+numbers near 1.000.000.000? Do your measurements confirm this?
+
+√10 is like 3.1, So yes the difference between (0.02 || 0.03) and (0.08) like 3 or 4. The next 
+references are also in range between 3 and 4 time bigger.
+
+2. Do the data for 100.000.000.000 and 1.000.000.000.000 support the √n prediction well?
+
+Yes, it is fine, but much slower than the initial calculations. I noticed that the difference 
+between the next pair of numbers will take about 3 times longer, so I suppose this algorithm can be improved.
+
+3. Is your result consistent with the assumption that programs on your machine take time proportional 
+to the number of steps?
+
+Yes, the program takes proportional number of time when I increase number.
 |#
 
 (define (smallest-divisor n) 
@@ -35,23 +79,27 @@
 )
 
 (define (timed-prime-test n)
-    (newline)
-    (display n)
     (start-prime-test n (runtime))
 )
 
 (define (start-prime-test n start-time)
     (if (prime? n)
-        (report-prime 
-            (- (runtime) start-time)
-        )
+        (report-prime n (- (runtime) start-time))
         (display "")
     )
 )
 
-(define (report-prime elapsed-time)
+(define (round-to-precision n precision)
+  (let* ((factor (expt 10 precision))
+         (rounded (/ (round (* n factor)) factor)))
+    (number->string rounded)
+  )
+)
+
+(define (report-prime n elapsed-time)
+    (display n)
     (display " *** ")
-    (display elapsed-time)
+    (display (round-to-precision elapsed-time 4))
     (newline)
 )
 
@@ -72,5 +120,11 @@
     )
 )
 
-(prepare-period 100000 100100)
 (newline)
+(prepare-period 1000000000 1000000100)
+(newline)
+(prepare-period 10000000000 10000000100)
+(newline)
+(prepare-period 100000000000 100000000100)
+(newline)
+(prepare-period 1000000000000 1000000000100)
