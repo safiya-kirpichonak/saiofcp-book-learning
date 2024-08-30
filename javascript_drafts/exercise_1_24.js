db2ddba5da9782a51bb73dd033654c90e16436ef
Sofia K. <sophiakirpichenok@gmail.com>
2024-08-30 20:29:25 +0200

    doing 1_24

--- javascript_drafts/exercise_1_24.js
+++ javascript_drafts/exercise_1_24.js
@@ -1,26 +1,35 @@
-function expmod(base, exp, mod) {
-  if (exp === 0) {
-    return 1;
-  } else if (exp % 2 === 0) {
-    const halfExp = expmod(base, exp / 2, mod);
-    return (halfExp * halfExp) % mod;
+/*
+  To check that one number (n) does not divide by the second number (a) use 
+  the following formula "Fermat's Little Theorem":
+
+  a ^ (n - 1) ≡ 1 (mod n)
+
+  The following algorithm indirectly uses this principle:
+  - doExponentiationModulo function calculates a ^ n mod n.
+  - doFermatTest checks is a ^ n mod n === a.
+*/
+
+function doExponentiationModulo(number, exponent, modulo) {
+  if (exponent === 0) return 1;
+
+  if (exponent % 2 === 0) {
+    const halfExponent = doExponentiationModulo(number, exponent / 2, modulo);
+    return (halfExponent * halfExponent) % modulo;
   } else {
-    return (base * expmod(base, exp - 1, mod)) % mod;
+    const evenExponent = doExponentiationModulo(number, exponent - 1, modulo);
+    return (number * evenExponent) % modulo;
   }
 }
 
-function fermatTest(n) {
-  function tryIt(a) {
-    return expmod(a, n, n) === a;
-  }
-  const a = 1 + Math.floor(Math.random() * (n - 1));
-  return tryIt(a);
+function doFermatTest(n) {
+  const randomNumber = 1 + Math.floor(Math.random() * (n - 1));
+  return doExponentiationModulo(randomNumber, n, n) === randomNumber;
 }
 
 function fastPrime(n, times) {
   if (times === 0) {
     return true;
-  } else if (fermatTest(n)) {
+  } else if (doFermatTest(n)) {
     return fastPrime(n, times - 1);
   } else {
     return false;
@@ -72,4 +81,10 @@ function isPrimeNumber(start, end) {
   }
 }
 
-searchForPrimesPeriod(2, 30);
+searchForPrimesPeriod(1000, 1100);
+searchForPrimesPeriod(10000, 10100);
+searchForPrimesPeriod(100000, 100100);
+searchForPrimesPeriod(1000000, 1000100);
+searchForPrimesPeriod(10000000, 10000100);
+searchForPrimesPeriod(100000000, 100000100);
+searchForPrimesPeriod(1000000000, 1000000100);
